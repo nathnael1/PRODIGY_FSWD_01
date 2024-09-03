a349706412759045a4b1c70324307187f19b96fa
nathnael1 <nathnaeltamirat3@gmail.com>
2024-09-03 20:45:59 +0300

    created user model'

--- secureUserAuthentication/authentication/models.py
+++ secureUserAuthentication/authentication/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    dob = models.DateField()
+
