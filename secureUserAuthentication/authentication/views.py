6eb19aa4562bb85553746d4b5300f9b178a6e31f
nathnael1 <nathnaeltamirat3@gmail.com>
2024-09-03 20:25:08 +0300

    UI complete

--- secureUserAuthentication/authentication/views.py
+++ secureUserAuthentication/authentication/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    return HttpResponseRedirect(reverse("login"))
+    return render(request,"unauthorized.html")
 def login(request):
     return render(request,"login.html")
 def register(request):
     return render(request,"register.html")
+
+
